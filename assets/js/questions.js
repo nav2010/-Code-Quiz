1f77eb9e946483e7c72e011a514b780bbfd1ab91
nav2010 <39226429+nav2010@users.noreply.github.com>
2023-01-20 12:27:13 +0000

    questions

---
+++ assets/js/questions.js
@@ -0,0 +1,8 @@
+const questions = [
+    {
+      questionText: "Commonly used data types DO NOT include:",
+      options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+      answer: "3. alerts",
+    },
+
+]
