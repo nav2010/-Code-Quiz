2a100ff9f999a439af9b4d192d929f4bede8e6a3
nav2010 <39226429+nav2010@users.noreply.github.com>
2023-01-21 15:35:55 +0000

    modi logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,33 +1,20 @@
+let startscreen = document.querySelector("#start-screen");
+let questionE1 = document.querySelector("#questions");
+let startBtn =document.querySelector("#start");
+let timeE1 = document.querySelector("#time");
 
-
-const startscreen = document.querySelector("#start-screen");
-const questions = document.querySelector("#questions");
-const startBtn=document.querySelector('#start');
-const time =document.querySelector("#time");
-let timerCount=60;
-let timer;
-
-
-// hide all 
-
-function hide(){
-    startscreen.setAttribute("hidden",true);
-    questions.setAttribute("hidden",true);
-    startBtn.setAttribute("hidden",true);
-}
 function quizstart(){
-  hide();
-  questions.removeAttribute("class");
-  timer=setInterval(()=>{
-    time.textContent=timerCount;
-    timerCount--;
-  
+
+  startscreen.setAttribute("class","hide");
+  questionE1.removeAttribute8("class");
+  timer = setInterval (() =>{
+
   },1000);
+
 }
 startBtn.addEventListener("click",quizstart);
 
-function retrievequation (){
+function retriveQuestions(){
 
+  
 }
-
-
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,17 @@
 const questions = [
     {
-      question: "Commonly used data types DO NOT include:",
-      choice: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+      title: "Commonly used data types DO NOT include:",
+      option: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
       answer: "3. alerts",
     },
     {
-        question:"The condition in an if/ else statement is enclosed within ______.",
-        choice:["1.quotes","2.curly brackets","3.Parentheses ","4.square brackets"],
+      title:"The condition in an if/ else statement is enclosed within ______.",
+      option:["1.quotes","2.curly brackets","3.Parentheses ","4.square brackets"],
         answer:"3.Parentheses"
     },
     {
-     question: "Arrays in JavaScript can be used to store ______.",
-     choice: [
+     title: "Arrays in JavaScript can be used to store ______.",
+     option: [
           "1. numbers and strings",
           "2. other arrays",
           "3. booleans",
@@ -20,15 +20,15 @@ const questions = [
         answer: "4. all of the above",
       },
     {
-    question:
+      title:
     "String values must be enclosed within _____ when being assigned to variables.",
-    choice: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    option: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
     answer: "3. quotes",
     },
     {
-    question:
+      title:
     "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choice: [
+    option: [
     "1. JavaScript",
     "2. terminal/bash",
     "3. for loops",
