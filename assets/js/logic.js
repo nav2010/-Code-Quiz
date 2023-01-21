77c991770e658da70e432b6b60cbabcafe05b0ba
nav2010 <39226429+nav2010@users.noreply.github.com>
2023-01-21 09:01:14 +0000

    modi logic

---
+++ assets/js/logic.js
@@ -0,0 +1,19 @@
+
+
+const startscreen = document.querySelector("#start-screen");
+const questions = document.querySelector("#questions");
+const startBtn=document.querySelector('#start');
+
+
+// hide all 
+
+function hide(){
+    startscreen.setAttribute("hidden",true);
+    questions.setAttribute("hidden",true);
+    startBtn.setAttribute("hidden",true);
+   
+    
+}
+
+
+
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,17 +1,17 @@
 const questions = [
     {
-      questionText: "Commonly used data types DO NOT include:",
-      options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+      question: "Commonly used data types DO NOT include:",
+      choice: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
       answer: "3. alerts",
     },
     {
-        questionText:"The condition in an if/ else statement is enclosed within ______.",
-        options:["1.quotes","2.curly brackets","3.Parentheses ","4.square brackets"],
+        question:"The condition in an if/ else statement is enclosed within ______.",
+        choice:["1.quotes","2.curly brackets","3.Parentheses ","4.square brackets"],
         answer:"3.Parentheses"
     },
     {
-        questionText: "Arrays in JavaScript can be used to store ______.",
-        options: [
+     question: "Arrays in JavaScript can be used to store ______.",
+     choice: [
           "1. numbers and strings",
           "2. other arrays",
           "3. booleans",
@@ -20,15 +20,15 @@ const questions = [
         answer: "4. all of the above",
       },
     {
-    questionText:
+    question:
     "String values must be enclosed within _____ when being assigned to variables.",
-    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    choice: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
     answer: "3. quotes",
     },
     {
-    questionText:
+    question:
     "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: [
+    choice: [
     "1. JavaScript",
     "2. terminal/bash",
     "3. for loops",
@@ -36,6 +36,4 @@ const questions = [
     ],
     answer: "4. console.log",
     },
-
-
 ]
