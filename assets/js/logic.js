0aa750553d3847dee40d156919fdc9ec131e94b2
nav2010 <39226429+nav2010@users.noreply.github.com>
2023-01-21 09:03:00 +0000

    modi logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,6 +3,9 @@
 const startscreen = document.querySelector("#start-screen");
 const questions = document.querySelector("#questions");
 const startBtn=document.querySelector('#start');
+const time =document.querySelector("#time");
+let timerCount=60;
+let timer;
 
 
 // hide all 
@@ -11,8 +14,6 @@ function hide(){
     startscreen.setAttribute("hidden",true);
     questions.setAttribute("hidden",true);
     startBtn.setAttribute("hidden",true);
-   
-    
 }
 
 
